d6716530ffdbf9c3153dd10023aef43c712e4229
Norbert Csibra <norbert.csibra@proemergotech.com>
2021-04-13 14:45:09 +0200

    Sync with gitlab version

--- stack.go
+++ stack.go
@@ -9,8 +9,6 @@ import (
 )
 
 // Frame represents a program counter inside a stack frame.
-// For historical reasons if Frame is interpreted as a uintptr
-// its value represents the program counter + 1.
 type Frame uintptr
 
 // pc returns the program counter for this frame;
